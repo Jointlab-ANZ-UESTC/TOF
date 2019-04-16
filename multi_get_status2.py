f4c4c0dbe60772a6b62785de6210533693d7dcfd
guyuhao <chenhuanyitan@qq.com>
2019-04-16 17:13:31 +0800

    no message

---
+++ multi_get_status2.py
@@ -0,0 +1,59 @@
+import multiprocessing
+import requests
+import csv
+import time
+
+
+def write_csv(written_file, content):
+        with open(written_file, "a", newline="") as f:
+            writer = csv.writer(f)
+            writer.writerow(content)
+
+
+def read_csv(read_file):
+    with open(read_file, "r") as f:
+        reader = csv.reader(f)
+        content = list(reader)
+        return content
+
+
+def get_status(url):
+    r = requests.get(url)
+    return [url, r.status_code]
+
+
+def process_write_status(url, written_file):
+    status_list = get_status(url)
+    write_csv(written_file, status_list)
+
+
+def main(read_path, out_path):
+    start_time = time.time()
+    urls = read_csv(read_path)
+    numbers_of_url = len(urls)
+    with open(out_path, "w"):
+        pass
+    pool = multiprocessing.Pool(4)
+    for i in range(numbers_of_url):
+        url = urls[i][0]
+        pool.apply_async(process_write_status, (url, out_path,))
+    pool.close()
+    pool.join()
+    return time.time()-start_time
+
+
+if __name__ == "__main__":
+    print(main("C:\\Users\\顾先生\\Desktop\\test.csv", "C:\\Users\\顾先生\\Desktop\\out3.csv"))
+# if __name__ == "__main__":
+#     start_time = time.time()
+#     urls = read_csv("C:\\Users\\顾先生\\Desktop\\test.csv")
+#     numbers_of_url = len(urls)
+#     with open("C:\\Users\\顾先生\\Desktop\\out.csv", "w"):
+#         pass
+#     pool = multiprocessing.Pool(4)
+#     for i in range(numbers_of_url):
+#         url = urls[i][0]
+#         pool.apply_async(process_write_status, (url, "C:\\Users\\顾先生\\Desktop\\out.csv",))
+#     pool.close()
+#     pool.join()
+#     print("Time use: {:f} seconds".format(time.time()-start_time))
